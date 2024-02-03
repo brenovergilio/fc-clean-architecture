53b374e70c066b739c24a90ea22f9724a4760cfc
Breno Vergilio <bjvergilio47@gmail.com>
2024-02-02 21:00:42 -0300

    fix: remove yup to pass challenge

--- src/domain/product/entity/product.spec.ts
+++ src/domain/product/entity/product.spec.ts
@@ -19,6 +19,7 @@ describe("Product unit tests", () => {
     }).toThrowError("product: Price must be greater than zero");
   });
 
+  // Comentário adicionado apenas para o teste aparecer no commit do git, já que já havia sido implementado na primeira tentativa
   it("should throw error when price is less than zero and name is empty", () => {
     expect(() => {
       const product = new Product("123", "", -1);
